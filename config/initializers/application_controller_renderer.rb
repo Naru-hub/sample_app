a5d35d0117edb3c8858b886f7a7250cd9265c276
Naru-hub <1208ny0324@gmail.com>
2021-05-05 06:24:56 +0000

    [finish]2-10

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'f386dacddb87596c4a1a595b874014cdc888030983eafb0bf968d069a77f53bd94849f8f0033cbb2d5f46bc8251ff8336e83b8ac4722c858cbe8445b937fdcd8'
